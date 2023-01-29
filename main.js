f8b3731861ff03124b8c78d403c162ecbb793d68
Aadi-kashyav <101461343+Aadi-kashyav@users.noreply.github.com>
2023-01-29 12:45:12 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
-game_status = "";
 
-
+/*created by prashant shukla */
+var rightWristX = "";
+var rightWristY = "";
+var scoreRightWrist = "";
 var paddle2 =10,paddle1=10;
 
 var paddle1X = 10,paddle1Height = 110;
 var paddle2Y = 685,paddle2Height = 70;
 
 var score1 = 0, score2 =0;
-var paddle1Y;
+var mouseY;
 
 var  playerscore =0;
-
+var audio1;
 var pcscore =0;
 //ball x and y and speedx speed y and radius
 var ball = {
@@ -20,105 +22,82 @@ var ball = {
     dx:3,
     dy:3
 }
-
-rightWristY = 0;
-rightWristX = 0;
-scoreRightWrist = 0;
-
-//Define a variable to hold the status of the game
-
- 
-
+function preload(){
+  ball_touch = loadSound("ball_touch_paddel.wav");
+  ball_missed = loadSound("missed.wav");
+}
 function setup(){
-var canvas =  createCanvas(700,600);
-canvas.parent('canvas');
+  var canvas =  createCanvas(700,600);
+  canvas.parent('canvas');
 
-video = createCapture(VIDEO);
-video.size(700, 600);
-video.hide();
+  video = createCapture(VIDEO);
+  video.size(600,300);
+  video.hide();
 
-poseNet = ml5.poseNet(video, modelLoaded);
-poseNet.on('pose', gotPoses);
+  poseNet = ml5.poseNet(video,modelLoaded);
+  poseNet.on('pose', gotPoses);
 }
-
-function modelLoaded() {
-  console.log('PoseNet Is Initialized');
+function modelLoaded(){
+  console.log('Model Loaded!');
 }
-
-function gotPoses(results)
-{
-  if(results.length > 0)
-  {
-
-    rightWristY = results[0].pose.rightWrist.y;
+function gotPoses(results){
+  if(results.length > 0){
+    console.log(results)
     rightWristX = results[0].pose.rightWrist.x;
-    scoreRightWrist =  results[0].pose.keypoints[10].score;
-    console.log(scoreRightWrist);
+    rightWristY = results[0].pose.rightWrist.y;
+    scoreRightWrist = results[0].pose.keypoints[10].score;
   }
 }
-
-function startGame()
-{
-   //Set the value of the status variable created in step 1 to “start”.
- //Update the h3 tag which we have created inside index.html file in project 138 who has id “status” to "Game Is Loaded”.
- game_status = "start";
- document.getElementById("status").innerHTML = "Gmae is Loaded";
-}
-
 function draw(){
-if(game_status == "start") // inside the if condition check if the game_status is equal to the value "start".
-{
-  background(0); 
-  image(video, 0, 0, 700, 600);
-
-  fill("black");
+ 
+ image(video,0,0,700,600);
+if(scoreRightWrist > 0.2){
+  console.log(scoreRightWrist);
+  fill("red");
   stroke("black");
-  rect(680,0,20,700);
+  circle(rightWristX , rightWristY , 20);
+}
+ fill("black");
+ stroke("black");
+ rect(680,0,20,700);
 
-  fill("black");
-  stroke("black");
-  rect(0,0,20,700);
+ fill("black");
+ stroke("black");
+ rect(0,0,20,700);
 
-  if(scoreRightWrist > 0.2)
-  {
-    fill("red");
-    stroke("red");
-    circle(rightWristX, rightWristY, 30);
-  }
-
-
-    //funtion paddleInCanvas call 
-    paddleInCanvas();
-        
-    //left paddle
-    fill(250,0,0);
+   //funtion paddleInCanvas call 
+   paddleInCanvas();
+ 
+   //left paddle
+   fill(250,0,0);
     stroke(0,0,250);
     strokeWeight(0.5);
-    paddle1Y = rightWristY; 
-    rect(paddle1X,paddle1Y,paddle1,paddle1Height,100);
-
-
+    mouseY; 
+   rect(paddle1,mouseY,paddle1,paddle1Height,100);
+   
+   
     //pc computer paddle
     fill("#FFA500");
     stroke("#FFA500");
-    var paddle2y =ball.y-paddle2Height/2;  rect(paddle2Y,paddle2y,paddle2,paddle2Height,100);
+   var paddle2y =ball.y-paddle2Height/2;  rect(paddle2Y,paddle2y,paddle2,paddle2Height,100);
     
     //function midline call
     midline();
     
     //funtion drawScore call 
-    drawScore();
-
-    //function models call  
-    models();
-
-    //function move call which in very important
+   drawScore();
+   
+   //function models call  
+   models();
+   
+   //function move call which in very important
     move();
-
-    }
-
-  }
-
+}
+function restart(){
+  pcscore = 0;
+  playerscore = 0;
+  loop();
+}
 
 
 //function reset when ball does notcame in the contact of padde
@@ -126,7 +105,8 @@ function reset(){
    ball.x = width/2+100,
    ball.y = height/2+100;
    ball.dx=3;
-   ball.dy =3;   
+   ball.dy =3;
+   
 }
 
 
@@ -153,7 +133,6 @@ function drawScore(){
     text(pcscore,555,50)
 }
 
-
 //very important function of this game
 function move(){
    fill(50,350,0);
@@ -166,15 +145,16 @@ function move(){
        ball.dx=-ball.dx-0.5;       
    }
   if (ball.x-2.5*ball.r/2< 0){
-  if (ball.y >= paddle1Y&& ball.y <= paddle1Y + paddle1Height) {
-    ball.dx = -ball.dx+0.5; 
-    
+  if (ball.y >= mouseY&& ball.y <= mouseY + paddle1Height) {
+    ball_touch.play();
+    ball.dx = -ball.dx+0.5;
+    playerscore++;
   }
   else{
     pcscore++;
-    
     reset();
     navigator.vibrate(100);
+    ball_missed.play();
   }
 }
 if(pcscore ==4){
@@ -183,12 +163,12 @@ if(pcscore ==4){
     rect(0,0,width,height-1);
     fill("white");
     stroke("white");
-    textSize(25);
-    text("Game Over!",width/2,height/2);
-    text("Reload the page!",width/2,height/2+30)
+    textSize(25)
+    text("Game Over!☹☹",width/2,height/2);
+    text("Press Restart button to play the game!",width/2,height/2+30)
     noLoop();
     pcscore = 0;
- }
+}
    if(ball.y+ball.r > height || ball.y-ball.r <0){
        ball.dy =- ball.dy;
    }   
@@ -208,13 +188,10 @@ function models(){
 
 //this function help to not go te paddle out of canvas
 function paddleInCanvas(){
-  if(paddle1Y+paddle1Height > height){
-    paddle1Y=height-paddle1Height;
+  if(mouseY+paddle1Height > height){
+    mouseY=height-paddle1Height;
   }
-  if(paddle1Y < 0){
-    paddle1Y =0;
-  }
- 
-  
+  if(mouseY < 0){
+    mouseY =0;
+  }  
 }
-
